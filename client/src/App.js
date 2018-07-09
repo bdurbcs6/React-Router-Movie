c7b56abc3bac169eed5416b0fb57f4baab1c6be9
bdurb <brian@rpmgreenville.com>
2018-07-09 12:42:49 -0700

    fixed route to movie by id

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <div>
           <Route exact path="/" component={MovieList} />
-          <Route path="/movies/movieid" component={Movie} />
+          <Route path="/movies/:id" component={Movie} />
         </div>
       </div>
     );
